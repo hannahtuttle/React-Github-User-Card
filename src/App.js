37739044fdc4231c24407a3f6aab8001ab05e6eb
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-08-06 15:06:34 -0700

    data rendering to page and working on styles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import UserCard from './components/userCard.js'
+
 class App extends React.Component {
   constructor(){
     super();
-    this.state = { user: {}}
+    this.state = { 
+      user: {},
+      followers: []
+  }
   }
 
   componentDidMount(){
     this.fetchUsers()
+    this.fetchFollowers()
   }
 
 fetchUsers = () => {
@@ -19,18 +25,32 @@ fetchUsers = () => {
   })
   .then(response => {
     //if(response === null){}
-   return this.setState({user: response}),
-   //console.log(this.state.user),
-    console.log(this.state.user)
+    return this.setState({user: response}),
+    console.log('this.state.user', this.state.user)
     })
   .catch((err) => 
     console.log(err))
 }
 
+fetchFollowers = () => {
+  fetch('https://api.github.com/users/hannahtuttle/followers')
+  .then(response => {
+    return response.json()
+  })
+  .then(response => {
+    return this.setState({followers: response}),
+    console.log("followers",this.state.followers)
+  })
+  .catch(err => console.log('error with followers api', err))
+}
+
   render(){
   return (
     <div className="App">
-     
+      <UserCard user={this.state.user}/>
+      {this.state.followers.map(person => 
+        <UserCard user={person}/>
+      )}
     </div>
   );
 }}
